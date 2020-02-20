723b672087e4e87da06e88e1548667cf096715bd
Scott Colson <scott@colsoncode.com>
2020-02-20 13:49:53 -0800

    Pushed CCLLC.CDS.Sdk v1.0.0

--- CCLLC.CDS/LibraryProjects/CCLLC.CDS.Sdk/Properties/AssemblyInfo.cs
+++ CCLLC.CDS/LibraryProjects/CCLLC.CDS.Sdk/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CCLLC.CDS.Sdk")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CCLLC.CDS.Sdk")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf336bb7-87c9-4561-9549-293426baaa3e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
