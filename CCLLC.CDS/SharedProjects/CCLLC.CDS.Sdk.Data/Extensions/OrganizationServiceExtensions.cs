ffc60e0ff078c37a433a7ce64cd3069d0eff6a88
Scott Colson <scott@colsoncode.com>
2020-03-30 09:13:35 -0700

    Add some additional data retrieval extensions.

--- CCLLC.CDS/SharedProjects/CCLLC.CDS.Sdk.Data/Extensions/OrganizationServiceExtensions.cs
+++ CCLLC.CDS/SharedProjects/CCLLC.CDS.Sdk.Data/Extensions/OrganizationServiceExtensions.cs
@@ -1,15 +1,56 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace CCLLC.CDS.Sdk
 {
     public static partial class Extensions
     {
+        /// <summary>
+        /// Execute a Fluent Query.
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="organizationService"></param>
+        /// <returns></returns>
         public static IExecutableFluentQuery<E> Query<E>(this IOrganizationService organizationService) where E : Entity, new()
         {
             var query = new ExecutableFluentQuery<E>(organizationService);
             return query;
         }
 
-        
+        /// <summary>
+        /// Retrieve a single early bound record with columns selected through projection.
+        /// </summary>        
+        public static T GetRecord<T>(this IOrganizationService organizationService, EntityReference recordId, Expression<Func<T, object>> anonymousTypeInitializer) where T : Entity
+        {
+            var columns = anonymousTypeInitializer.GetAttributeNamesArray<T>();
+
+            return organizationService.GetRecord<T>(recordId, columns);
+        }
+
+        /// <summary>
+        /// Retrieve a single early bound record with columns identified as string parameters.
+        /// </summary> 
+        public static T GetRecord<T>(this IOrganizationService organizationService, EntityReference recordId, params string[] columns) where T : Entity
+        {
+            return organizationService.GetRecord(recordId, columns).ToEntity<T>();
+        }
+
+        /// <summary>
+        /// Retrieve an entity record with columns identified as string parameters.
+        /// </summary> 
+        public static Entity GetRecord(this IOrganizationService organizationService, EntityReference recordId, params string[] columns)
+        {
+            var columnSet = columns == null ? new ColumnSet(true) : new ColumnSet(columns);
+
+            var record = organizationService.Retrieve(
+                recordId.LogicalName,
+                recordId.Id,
+                columnSet);
+
+            return record;
+        }
+
     }
 }
