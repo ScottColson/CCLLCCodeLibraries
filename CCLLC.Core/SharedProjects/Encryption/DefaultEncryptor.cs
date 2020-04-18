4c2610e32c2f6778462290155187d7efc37467ea
Scott Colson <scott@colsoncode.com>
2020-04-18 09:52:43 -0700

    Updates to encryption service to make key optional for implementations that provide a default key. Made DefaultEncryptor inheritable. Pushed CCLLC.Core.Encryption 1.0.1

--- CCLLC.Core/SharedProjects/Encryption/DefaultEncryptor.cs
+++ CCLLC.Core/SharedProjects/Encryption/DefaultEncryptor.cs
@@ -10,22 +10,25 @@ namespace CCLLC.Core
     /// </summary>
     public class DefautlEncryptor : IEncryptionService
     {
-        private static readonly string defaultKey = "7a5a64brEgaceqenuyegac7era3Ape6aWatrewegeka94waqegayathudrebuc7t";
-        private readonly byte[] salt = { 73, 118, 97, 110, 32, 77, 101, 100, 118, 101, 100, 101, 118 };
+        protected string DefaultKey { get; set; }
+        protected byte[] Salt { get; set; }
+      
+        public DefautlEncryptor()
+        {
+            DefaultKey = "7a5a64brEgaceqenuyegac7era3Ape6aWatrewegeka94waqegayathudrebuc7t";
+            Salt = new byte[] { 73, 118, 97, 110, 32, 77, 101, 100, 118, 101, 100, 101, 118 };
+        }
 
-        public static string DefaultKey => defaultKey;
-        
-
-        public string Decrypt(string cipherText, string Password)
+        public virtual string Decrypt(string cipherText, string key = null)
         {
             byte[] cipherData = Convert.FromBase64String(cipherText);
-            byte[] clearData = this.Decrypt(cipherData, Password);
+            byte[] clearData = this.Decrypt(cipherData, key ?? DefaultKey);
             return Encoding.Unicode.GetString(clearData);
         }
 
-        public byte[] Decrypt(byte[] cipherData, string Password)
+        public virtual byte[] Decrypt(byte[] cipherData, string key = null)
         {
-            PasswordDeriveBytes passwordDeriveByte = new PasswordDeriveBytes(Password, salt);
+            PasswordDeriveBytes passwordDeriveByte = new PasswordDeriveBytes(key ?? DefaultKey, Salt);
             return this.Decrypt(cipherData, passwordDeriveByte.GetBytes(32), passwordDeriveByte.GetBytes(16));
         }
 
@@ -41,26 +44,26 @@ namespace CCLLC.Core
             return memoryStream.ToArray();
         }
 
-        public string Encrypt(string clearText, string Password)
+        public virtual string Encrypt(string clearText, string key = null)
         {
             byte[] clearData = Encoding.Unicode.GetBytes(clearText);
-            byte[] encryptedData = this.Encrypt(clearData, Password);
+            byte[] encryptedData = this.Encrypt(clearData, key ?? DefaultKey);
             return Convert.ToBase64String(encryptedData);
         }
 
-        public byte[] Encrypt(byte[] clearData, string Password)
+        public virtual byte[] Encrypt(byte[] clearData, string key = null)
         {
-            PasswordDeriveBytes passwordDeriveByte = new PasswordDeriveBytes(Password, salt);
+            PasswordDeriveBytes passwordDeriveByte = new PasswordDeriveBytes(key ?? DefaultKey, Salt);
             return this.Encrypt(clearData, passwordDeriveByte.GetBytes(32), passwordDeriveByte.GetBytes(16));
         }
 
-        private byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV)
+        private byte[] Encrypt(byte[] clearData, byte[] key, byte[] vector)
         {
             MemoryStream memoryStream = new MemoryStream();
-            Rijndael key = Rijndael.Create();
-            key.Key = Key;
-            key.IV = IV;
-            CryptoStream cryptoStream = new CryptoStream(memoryStream, key.CreateEncryptor(), CryptoStreamMode.Write);
+            Rijndael encryptor = Rijndael.Create();
+            encryptor.Key = key;
+            encryptor.IV = vector;
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor.CreateEncryptor(), CryptoStreamMode.Write);
             cryptoStream.Write(clearData, 0, (int)clearData.Length);
             cryptoStream.Close();
             byte[] array = memoryStream.ToArray();
--- CCLLC.Core/SharedProjects/Encryption/Interfaces/IEncryptionService.cs
+++ CCLLC.Core/SharedProjects/Encryption/Interfaces/IEncryptionService.cs
@@ -6,35 +6,39 @@
     public interface IEncryptionService
     {
         /// <summary>
-        /// Encrypt a clear text string value into an encrypted string using the supplied encryption key.
+        /// Encrypt a clear text string value into an encrypted string using the supplied encryption key. 
+        /// If a key is not provided, decryption is based on the implementations default key value.
         /// </summary>
         /// <param name="clearText"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        string Encrypt(string clearText, string key);
+        string Encrypt(string clearText, string key = null);
 
         /// <summary>
-        /// Encrypt a clear data byte array value into an encrypted byte array using the supplied encryption key.
+        /// Encrypt a clear data byte array value into an encrypted byte array using the supplied encryption
+        /// key. If a key is not provided, decryption is based on the implementations default key value.
         /// </summary>
         /// <param name="clearData"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        byte[] Encrypt(byte[] clearData, string key);
+        byte[] Encrypt(byte[] clearData, string key = null);
 
         /// <summary>
-        /// Decrypt an encrypted string value using the supplied key and return clear text.
+        /// Decrypt an encrypted string value using the supplied key and return clear text. If a key is not 
+        /// provided, decryption is based on the implementations default key value.
         /// </summary>
         /// <param name="encryptedText"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        string Decrypt(string encryptedText, string key);
+        string Decrypt(string encryptedText, string key = null);
 
         /// <summary>
         /// Decrypt an encrypted byte array using the supplied key and return clear byte array.
+        /// If a key is not provided, decryption is based on the implementations default key value.
         /// </summary>
         /// <param name="encryptedData"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        byte[] Decrypt(byte[] encryptedData, string key);
+        byte[] Decrypt(byte[] encryptedData, string key = null);
     }
 }
