1a6b1540ec9a395a025da9fcc943d5be75a89734
Scott Colson <scott@colsoncode.com>
2020-05-30 10:20:38 -0700

    bug fix interface scope.

--- CCLLC.CDS/SharedProjects/CCLLC.CDS.Sdk.Metadata/Interfaces/ISdkMessageMetadataService.cs
+++ CCLLC.CDS/SharedProjects/CCLLC.CDS.Sdk.Metadata/Interfaces/ISdkMessageMetadataService.cs
@@ -3,7 +3,7 @@ using Microsoft.Xrm.Sdk;
 
 namespace CCLLC.CDS.Sdk.Metadata
 {
-    interface ISdkMessageMetadataService
+    public interface ISdkMessageMetadataService
     {
         IEnumerable<string> GetSdkMessageNames(IOrganizationService orgService, bool includeNonVisibleMessages = false);
         IEnumerable<SdkMessageMetadata> GetSdkMessageMetadata(IOrganizationService orgService, IEnumerable<string> messageNames);
